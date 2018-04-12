16e7566da339d59b728da417966f0eade7da73db
Ernesto Estrada <eestrad9@msudenver.edu>
2018-04-12 15:23:02 -0600

    Initial commit

---
+++ app/src/main/java/com/ernestoestrada/mapmyrun/MapsActivity.kt
@@ -0,0 +1,74 @@
+package com.ernestoestrada.mapmyrun
+
+import android.Manifest.*
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import org.jetbrains.anko.toast
+import java.util.jar.Manifest
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private val LOCATION_REQUEST_CODE = 101
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun requestPermission(PermissionType: String, requestCode: Int) {
+        ActivityCompat.requestPermissions(this, arrayOf(PermissionType), requestCode)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>, grantResults: IntArray) {
+
+        when (requestCode) {
+            LOCATION_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    toast("Unable to show location - permission required")
+                } else {
+                    val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+                    mapFragment.getMapAsync(this)
+                }
+            }
+        }
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        if (mMap != null) {
+            val isOkay = ContextCompat.checkSelfPermission(this,
+                    permission.ACCESS_FINE_LOCATION)
+            if (isOkay == PackageManager.PERMISSION_GRANTED) {
+                mMap?.isMyLocationEnabled = true
+            } else {
+                requestPermission(permission.ACCESS_FINE_LOCATION, LOCATION_REQUEST_CODE)
+            }
+        }
+
+    }
+}
